3a0b1e209636405ec67830a6a78d2c54b16d3767
monkphin <darren.burrows@gmail.com>
2022-01-23 19:38:52 +0000

    refactored app.py to reduce the amount of DB calls being made, since this can be set globally. Updated app.py index.html, edit.html and delete.html to further implement CRUD functionality

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename #Needed for future functionality
 from werkzeug.datastructures import  FileStorage #Needed for future functionality
@@ -12,6 +12,7 @@ app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_HOST'] = 'crud_db'
 app.config['MYSQ_PORT'] = 3306
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_DB'] = 'newsstand_db'
 
 app.testing = True
 
@@ -31,7 +32,6 @@ def allowed_file(filename):
 @app.route("/home", methods=['POST','GET'])
 def index():
     cursor = mysql.connection.cursor()
-    cursor.execute(f"use newsstand_db;")
     cursor.execute(f"SELECT * FROM `sites`;")
     cursor.execute(f"SELECT news.*, ed.* FROM sites AS news, editors AS ed WHERE ed.news_id = news.news_id;")
     site_list = cursor.fetchall()
@@ -46,60 +46,86 @@ def add():
     if request.method == 'POST':
         if request.form['name'] != "" and  request.form['url'] != "": 
             cursor = mysql.connection.cursor()
-            cursor.execute(f"use newsstand_db;")
             Name = request.form['name']
             Site = request.form['url']
             ed_firstname = request.form['ed_firstname']
             ed_lastname = request.form['ed_lastname']
             ed_datestarted = request.form['ed_datestarted']
             cursor.execute(f"INSERT INTO `sites`  (`news_name`, `news_url`) VALUES ('{Name}','{Site}');")
-            mysql.connection.commit()                 
+            mysql.connection.commit()     
+
+            '''     Begin hacky method to autopopulate the foreign key in editors     '''      
             cursor.execute(f" SELECT news_id from `sites` WHERE  news_name = '{Name}'")
             News_id = cursor.fetchone ()
             News_int = News_id["news_id"]
             cursor.execute(f"INSERT INTO `editors` (news_id, ed_fname, ed_lname, ed_date) VALUES ('{News_int}', '{ed_firstname}', '{ed_lastname}', '{ed_datestarted}');")
             mysql.connection.commit()                                            
             cursor.close()
+            '''     End hacky method to autopopulate the foreign key in editors      '''      
+
         return render_template("add.html")
 
     return render_template("add.html")
 
 
-@app.route('/edit/<id>', methods=['GET', 'POST'])
+@app.route('/sites/<id>/edit', methods=['GET', 'POST'])
 def edit(id):
-    if request.method == 'POST':
-        Name= request.form['name']
-        Site = request.form['url']
+ #GET is to render out put into fields to show what you're currently editing. Currently not working as expected.  
+    if request.method == 'GET':
+       
         cursor = mysql.connection.cursor()
-        cursor.execute(f"use newsstand_db;") 
-        cursor.execute(f"UPDATE `sites` SET `news_name` = '{Name}', `news_url` = '{Site}' WHERE `sites`.`news_id` = '{id}'") 
-        mysql.connection.commit()                                            
+        cursor.execute(f"SELECT * FROM `sites` WHERE `sites`.`news_id` = '{id}';")
+        local_edit_list = cursor.fetchall()
+     
+
+        return render_template('/edit.html', id=id, edit_list=local_edit_list)
+
+    if request.method == 'POST':
+
+        new_site_url = request.form['new_name']
+        new_site_name = request.form['new_url']
+        ed_fname = request.form['ed_f']
+        ed_lname = request.form['ed_l']
+        ed_date = request.form['ed_d']
+        cursor = mysql.connection.cursor()
+        cursor.execute(f"UPDATE `sites` SET `news_name`='{new_site_name}', `news_url`='{new_site_url}' WHERE `sites`.`news_id`='{id}';") 
+        cursor.execute(f"UPDATE `editors` SET `ed_fname`='{ed_fname}', `ed_lname`='{ed_lname}', `ed_date`='{ed_date}' WHERE `editors`.`news_id`='{id}';") 
+        mysql.connection.commit()                                          
         cursor.close()
-    return render_template('/edit.html', id=id)
+
+        return redirect('/home')
+    
 
 
-@app.route('/delete/<id>', methods=['GET', 'POST'])
+
+@app.route('/sites/<id>/delete', methods=['GET', 'POST'])
 def delete(id):
+
+    if request.method == 'GET':
+
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(f"SELECT * FROM `sites` WHERE `sites`.`news_id` = '{id}';")
+        local_delete_list = cursor.fetchall()
+     
+
+
+        return render_template('/delete.html', id=id, delete_list=local_delete_list)
+
     if request.method == 'POST':
         cursor = mysql.connection.cursor()
-        cursor.execute(f"use newsstand_db;")  
-        cursor.execute(f"SELECT news_name FROM sites WHERE news_id = {'id'}") 
-        list = cursor.fetchall()
-        cursor.close
-        cursor = mysql.connection.cursor()
-        cursor.execute(f"use newsstand_db;") 
-        cursor.execute(f"DELETE FROM `sites` WHERE `sites`.`news_id` = {'id'}'")
-        mysql.connection.commit()                                            
+        cursor.execute(f"DELETE FROM `editors` WHERE `news_id` = '{id}';")
+        cursor.execute(f"DELETE FROM `sites` WHERE `sites`.`news_id` = '{id}';")
+        mysql.connection.commit()                                          
         cursor.close()
-    return render_template('/delete.html', list=list, id=id)
 
-#to iumplement - for uploading of custom images. 
+        return redirect('/home')
+    
+'''
 @app.route('/uploader', methods = ['GET', 'POST'])
 def uploader():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+
+'''
 
 @app.route("/help")
 def help():
